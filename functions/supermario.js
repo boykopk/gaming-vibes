fb64902beacdd9cb13e203d2ce54377a90204007
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2021-04-29 11:19:57 +0300

    super mario function fixed data

--- functions/supermario.js
+++ functions/supermario.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
   console.log('function ran');
 
-  const data = { name: mario, age: 35, job: plumber };
+  const data = { name: 'mario', age: 35, job: 'plumber' };
 
   // return response to browser
   return {
